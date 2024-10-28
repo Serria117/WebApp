0f6aff20982493bf4cb9d64b81738b6627b9b5b9
Dth256 <june25th87@gmail.com>
2024-10-28 16:26:03 +0700

    Add notification service

--- SignalrConfig/AppHub.cs
+++ SignalrConfig/AppHub.cs
@@ -1,8 +1,29 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using WebApp.Core.DomainEntities;
+using WebApp.Services.NotificationService;
 
 namespace WebApp.SignalrConfig;
 
 [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-public class AppHub : Hub;
+public class AppHub(INotificationAppService notificationService) : Hub
+{
+    public override Task OnConnectedAsync()
+    {
+        var userId = Context.UserIdentifier;
+        if (userId is not null)
+        {
+            notificationService.RegisterConnection(userId, Context.ConnectionId);
+        }
+        return base.OnConnectedAsync();
+    }
+    
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        var userId = Context.UserIdentifier;
+        if (userId is not null) 
+            notificationService.UnregisterConnection(userId);
+        return base.OnDisconnectedAsync(exception);
+    }
+};
